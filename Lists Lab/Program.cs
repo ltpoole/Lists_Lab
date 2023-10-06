76fe2a2305e485d8ab7a58d70918b99809e18cc4
ltpoole <laborris.poole@gmail.com>
2023-10-05 20:36:34 -0400

    Uploading Lists Lab to online repo

---
+++ Lists Lab/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace Lists_Lab
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+         List<string> programs = new List<string> { "C#", "JavaScript", "Python", "Java", "ASP.NET", "React" };
+
+         List<string> instructors = new List<string> { "Mike", "Nevin", "Jake", "Dan", "Megan" };
+            instructors.Add("Dan");
+            instructors.Add("Amy");
+            instructors.Add("Ted");
+         
+           
+
+        }
+    }
+}
